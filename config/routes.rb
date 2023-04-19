6f52cc8595fa6bc76b601bcb40f98b2a1c082ed9
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-04-19 12:14:18 +0300

    COA-3 fix: add named routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'static_pages/home', to: 'static_pages#home', as: 'home'
+  get 'static_pages/help', to: 'static_pages#help',  as: 'help'
+  get 'static_pages/about', to: 'static_pages#about', as: 'about'
+  # get 'static_pages/contact', to: 'static_pages#contact', as: 'contact'
+  # get 'static_pages/signup', to: 'static_pages#signup', as: 'signup'
+  # get 'static_pages/login', to: 'static_pages#login', as: 'login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
