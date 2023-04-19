052db48e3db3c44f732a6c783b844898f38f8d8a
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-04-19 12:29:28 +0300

    COA-3 feat: shorten URL paths i.e /static_pages/help to /help.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home', to: 'static_pages#home', as: 'home'
-  get 'static_pages/help', to: 'static_pages#help',  as: 'help'
-  get 'static_pages/about', to: 'static_pages#about', as: 'about'
-  get 'static_pages/contact', to: 'static_pages#contact', as: 'contact'
-  # get 'static_pages/signup', to: 'static_pages#signup', as: 'signup'
-  # get 'static_pages/login', to: 'static_pages#login', as: 'login'
+  get '/home', to: 'static_pages#home', as: 'home'
+  get '/help', to: 'static_pages#help',  as: 'help'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  # get '/signup', to: 'static_pages#signup', as: 'signup'
+  # get '/login', to: 'static_pages#login', as: 'login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
