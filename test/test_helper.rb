0575c502ee3fc970a98a21972e9f01bd0776c4c1
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-04-25 08:31:21 +0300

    COA-6 fix: login upon signup. - newly registered users might be confused, as they are not logged in by default. - add a call to 'reset_session' immediately before 'logging in' to guard against the 'session-fixation' attacks.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require_relative "../config/environment"
 require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
@@ -11,4 +12,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
