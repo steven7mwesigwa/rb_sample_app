19baef7c62269a44ecf9c43ddc47e73f24d1d87f
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-04-27 13:08:17 +0300

    COA-7 feat: add a 'Remember Me' Checkbox.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,4 +17,20 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Our method for logging a user in depends on the type of test.
+  # Inside controller tests, we can manipulate the session method directly.
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Inside integration tests, we can’t manipulate session directly,
+  # but we can 'post' to the 'sessions' path.
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
+  end
 end
