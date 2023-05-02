2bdf10e5ecafa69353a3460f9050cadabf8f9833
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-05-02 15:47:03 +0300

    COA-8 feat: test successful edits on the User model 'edit' HTML form.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+
 class UsersEditTest < ActionDispatch::IntegrationTest
 
   def setup
@@ -19,5 +20,30 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
     assert_template 'users/edit'
 
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+
+    assert_template 'users/edit'
+
+    name = "Foo Bar"
+    email = "foo@bar.com"
+
+    patch user_path(@user), params: {
+      user: {
+        name: name,
+        email: email,
+        password: "",
+        password_confirmation: ""
+      } }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+
+    @user.reload
+
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
