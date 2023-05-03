af31bb18256aae0803804b6cf443303605c28530
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-05-03 20:28:52 +0300

    COA-9 feat: add tests for sending 'user account activation' emails.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "user@realdomain.com"
+  default from: "stevekats1356@gmail.com"
   layout "mailer"
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,9 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 end
