c96c12da9d56dab58bb8a4668dcb309000fc4da7
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-05-03 08:36:45 +0300

    COA-9 feat: set up the 'Activation Token Callback'.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -46,6 +48,17 @@ class User < ApplicationRecord
   # We reuse the remember digest for convenience.
   def session_token
     remember_digest || remember
+  end
+
+  # Converts email to all lowercase.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 
 end
