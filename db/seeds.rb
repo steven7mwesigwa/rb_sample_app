df8e78524aae83e9db2b6c30328ba4fa56442862
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-05-03 04:49:18 +0300

    COA-8 feat: add 'admin' column to 'users' database table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@ User.create!(
   name: "Example User",
   email: "example@railstutorial.org",
   password: "foobar",
-  password_confirmation: "foobar"
+  password_confirmation: "foobar",
+  admin: true
 )
 
 # Generate a bunch of additional users.
