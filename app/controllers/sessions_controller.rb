659c4202a818911c61a8dbcb3ab2d9852077b713
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-05-03 03:35:43 +0300

    COA-8 feat: add a show all users 'GET: /users' page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       reset_session
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
-      
+
       redirect_to forwarding_url || user
     else
       # Create an error message.
