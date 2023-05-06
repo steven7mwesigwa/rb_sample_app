4d72f7f988c5137e1227ed9292174af66b8df811
Steven Mwesigwa <steven.mwesigwa@tollandhaben.com>
2023-05-06 16:33:54 +0300

    COA-9 feat: prevent unactivated users from logging in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,24 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
+      if user.activated?
 
-      forwarding_url = session[:forwarding_url]
+        forwarding_url = session[:forwarding_url]
 
-      # Log the user in and redirect to the user's show page.
-      reset_session
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      log_in user
+        # Log the user in and redirect to the user's show page.
+        reset_session
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        log_in user
 
-      redirect_to forwarding_url || user
+        redirect_to forwarding_url || user
+      else
+
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+
+        redirect_to root_url
+      end
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
